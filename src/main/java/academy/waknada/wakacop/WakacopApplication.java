3b958caa50fd2762f091bbb9a8ae56afcc4a2116
Lucas Aquino <lucasaquino350@gmail.com>
2023-10-16 21:33:51 -0300

    init

---
+++ src/main/java/academy/waknada/wakacop/WakacopApplication.java
@@ -0,0 +1,13 @@
+package academy.waknada.wakacop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WakacopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WakacopApplication.class, args);
+	}
+
+}
